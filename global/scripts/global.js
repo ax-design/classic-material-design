6d0de1593387f5462468a06373b0e023f320599e
UTL_1138 <utl1138@outlook.com>
2022-06-07 17:14:20 +0800

    转移部分涟漪效果实现方式

--- global/scripts/global.js
+++ global/scripts/global.js
@@ -153,7 +153,6 @@ document.querySelectorAll('*[interact], button,footer>a').forEach((element) => {
 
 function rippleForeground(element, event) {
     let unboundedCondition = element.hasAttribute('unbounded') ? true : false;
-    let rippleExpandDuration = unboundedCondition ? 400 : 800;
     let rippleMoveParameter = unboundedCondition ? 0 : 0.7;
 
     const containerWidth = element.getBoundingClientRect().width;
@@ -166,17 +165,17 @@ function rippleForeground(element, event) {
     nodes[nodes.length - 1].appendChild(rippleForegroundObject);
     let lastRippleForeground = nodes[nodes.length - 1].querySelector('div:last-child');
 
-    if (unboundedCondition == false) {
+    if (!unboundedCondition) {
         const rippleRadius = Math.min(containerRadius, 350) * 0.9 + containerRadius * Math.random() * 0.1;
-        lastRippleForeground.attributeStyleMap.set('width', CSS.px(rippleRadius * 2));
+        lastRippleForeground.attributeStyleMap.set('inline-size', CSS.px(rippleRadius * 2));
     }
 
     if (event.type !== 'keydown') {
         const rippleOffsetX = event.clientX - element.getBoundingClientRect().left - containerWidth / 2;
         const rippleOffsetY = event.clientY - element.getBoundingClientRect().top - containerHeight / 2;
         lastRippleForeground.animate([
-            { transform: new CSSTranslate(CSS.px(rippleOffsetX), CSS.px(rippleOffsetY)) },
-            { transform: new CSSTranslate(CSS.px(rippleOffsetX * rippleMoveParameter), CSS.px(rippleOffsetY * rippleMoveParameter)) }
+            { translate: new CSSUnparsedValue([CSS.px(rippleOffsetX), ' ', CSS.px(rippleOffsetY)]) },
+            { translate: new CSSUnparsedValue([CSS.px(rippleOffsetX * rippleMoveParameter), ' ', CSS.px(rippleOffsetY * rippleMoveParameter)]) }
         ], {
             duration: 300,
             easing: 'cubic-bezier(0.17, 0.74, 0.4, 1)',
@@ -185,14 +184,8 @@ function rippleForeground(element, event) {
         });
     }
 
-    lastRippleForeground.animate({
-        transform: ['scale(0)', 'scale(1)']
-    }, {
-        duration: rippleExpandDuration,
-        easing: 'cubic-bezier(0.17, 0.74, 0.4, 1)',
-        fill: 'both',
-        composite: 'add'
-    }).onfinish = () => {
+    lastRippleForeground.addEventListener('animationend', () => {
         lastRippleForeground.remove();
-    };
+    });
+
 }
